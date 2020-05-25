787188a623d70e04fb474c2dae14624835f8ee64
Nathalie Pillen <56022155+NathaliePillen@users.noreply.github.com>
2020-05-25 18:26:37 +0200

    mapping components

--- react-props-practice/src/components/App.jsx
+++ react-props-practice/src/components/App.jsx
@@ -4,12 +4,25 @@ import contacts from "../contacts";
 import Card from "./Card";
 import Avatar from "./Avatar";
 
+function createCard(contacts){
+  return <Card
+  key={contacts.id}
+  id={contacts.id}
+  name={contacts.name}
+  img={contacts.imgURL}
+  tel={contacts.phone}
+  email={contacts.email}
+/>
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://media-exp1.licdn.com/dms/image/C5603AQEo8XrQWnXoAg/profile-displayphoto-shrink_200_200/0?e=1594252800&v=beta&t=IAPWD7T8jcYJG3IDxgQJDr6djFUJK1nXVh6lKYOQxWU"/>
-      <Card
+      
+      {contacts.map(createCard)}
+      {/* { <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -26,7 +39,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> } */}
     </div>
   );
 }
